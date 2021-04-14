6050616fccd76c944dc4fa4671d419075fdf6d4f
Kiana Long <kianajade42.kl@gmail.com>
2021-04-14 20:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
 function deleteFromDriverByKey (driver,key){
   const newObj = Object.assign({}, driver);
   delete newObj[key];
+  return newObj;
+}
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key];
+  return driver;
 }
